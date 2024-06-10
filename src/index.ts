43d031b9a3a1b01a7bf06fca366835c9ee194c68
Konotorii <79553360+konotorii@users.noreply.github.com>
2024-06-10 14:13:46 -0400

    fix: Functions can be imported from tools and individually (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+import {name as _delay} from './utility/src/delay';
+import {name as _formatNumber} from './utility/src/formatNumber';
+
+export const delay = _delay;
+export const formatNumber = _formatNumber;
 export * as tools from './utility';
