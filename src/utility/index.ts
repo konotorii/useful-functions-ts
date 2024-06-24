33f19fce29cf7dce17e9d179c41266d9c97dea0c
Konotorii <79553360+konotorii@users.noreply.github.com>
2024-06-24 15:26:53 -0400

    feat: Export logger class

--- src/utility/index.ts
+++ src/utility/index.ts
@@ -1,7 +1,9 @@
 import {name as _delay} from './src/delay';
 import {name as _formatNumber} from './src/formatNumber';
 import {name as _percentage} from './src/percentage';
+import {default as _logger} from './src/logger';
 
 export const delay = _delay;
 export const formatNumber = _formatNumber;
 export const percentage = _percentage;
+export const logger = _logger;
