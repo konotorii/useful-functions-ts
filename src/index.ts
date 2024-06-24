bfaaa384472ef3e086c734c421c7a20051de0042
Konotorii <79553360+konotorii@users.noreply.github.com>
2024-06-24 15:27:19 -0400

    fix: Potentially fix the exports to be less space consuming

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-import {name as _delay} from './utility/src/delay';
-import {name as _formatNumber} from './utility/src/formatNumber';
-
-export const delay = _delay;
-export const formatNumber = _formatNumber;
+export * from './utility'
 export * as tools from './utility';
