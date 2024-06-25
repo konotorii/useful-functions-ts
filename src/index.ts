b7c81e0cbc58f09eef71f710ecec1c3dc946e3a1
Konotorii <79553360+konotorii@users.noreply.github.com>
2024-06-25 08:22:18 -0400

    fix: Individual imports work again

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+import {name as _delay} from './utility/src/delay';
+import {name as _formatNumber} from './utility/src/formatNumber';
+import {name as _percentage} from './utility/src/percentage';
+import {default as _logger} from './utility/src/logger';
+
+export const delay = _delay;
+export const formatNumber = _formatNumber;
+export const percentage = _percentage;
+export const Logger = _logger;
 export * as tools from './utility';
